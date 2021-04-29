37c8e48a6bf5579852dab9c545f3a2de7e03b021
MohamedEl-mowafy <80730320+MohamedEl-mowafy@users.noreply.github.com>
2021-04-29 23:50:41 +0200

    splitter app

---
+++ splitter/splitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splitter
+//
+//  Created by Mohamed Elmowafy on 29/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
